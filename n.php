0ebc50c7ff94088cbcecc463cc934302fb235256
17mohan <30790741+mohan345@users.noreply.github.com>
2018-02-05 18:12:46 +0530

    Add files via upload

---
+++ n.php
@@ -0,0 +1,15 @@
+<?php
+$name = 'alex';
+$age = 21;
+if ($name===strolower('alex')) {
+	if ($age>=21) {
+		echo 'you\re over 21';
+		if (1===1) { 
+			echo 'yes, 1 is equal to 1';
+		}
+	}
+}
+else {
+	echo 'you\'re not alex';
+}
+?>
